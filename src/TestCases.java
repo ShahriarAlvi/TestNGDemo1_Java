a486a9e1788f3fc45d7069156cdb25ad0f12ba57
Shahriar Alvi <alam.shahriaralvi@gmail.com>
2022-05-27 04:45:44 +0600

    Initial commit

---
+++ src/TestCases.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    void setup(){
+        System.out.println("inside setup");
+    }
+    @Test
+    void testSteps(){
+        System.out.println("inside teststeps");
+    }
+    @Test
+    void tearDown(){
+        System.out.println("inside teardown");
+    }
+}
